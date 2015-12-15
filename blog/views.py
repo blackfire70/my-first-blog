f64bd6aacce9107fbccb39168a80fd9328795569
Miguel Concepcion <miguel.concepcion@outlook.com>
2015-12-15 11:44:01 +0800

    removed postlist

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ import time
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+#def post_list(request):
+#    posts = Post.objects.filter(published_date__lte=time.timezone.now()).order_by('published_date')
+    
+#    return render(request, 'blog/post_list.html', {})
